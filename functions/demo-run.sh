87ab9d3eaa5675d981a5ad0b7da111feb00a5974
Aditya Padhye <apadhye@pivotal.io>
2017-06-12 11:02:28 -0700

    Changing ports to resolve conflicts with other apps running on port 8080

--- functions/demo-run.sh
+++ functions/demo-run.sh
@@ -4,16 +4,17 @@ mvn package -DskipTests | grep BUILD
 ls -l | grep functions
 
 printf "\n"
-printf "### Adding data from blue-app (localhost:8080) - Will store objects which have just the name field\n"
-curl -X PUT localhost:8080/person/1
-curl -X PUT localhost:8080/person/2
+printf "### Adding data from blue-app (localhost:8081) - Will store objects which have just the name field\n"
+curl -X PUT localhost:8081/person/1
+curl -X PUT localhost:8081/person/2
 
 printf "\n\n"
 printf "### Deploying Jar to Gemfire cluster and assigning cache-writer to region /Person\n"
-export PATH=/Users/apadhye/Pivotal/GemFire/pivotal-gemfire-9.0.1/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+#export PATH=/Users/apadhye/Pivotal/GemFire/pivotal-gemfire-9.0.1/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 gfsh <<EOF
 connect --locator=localhost[41111]
-deploy --jar=/Users/apadhye/Pivotal/GemFire/gemfire-blue-green-deploy/functions/target/functions-1.0-SNAPSHOT.jar
+#deploy --jar=/Users/apadhye/Pivotal/GemFire/gemfire-blue-green-deploy/functions/target/functions-1.0-SNAPSHOT.jar
+deploy --jar=./target/functions-1.0-SNAPSHOT.jar
 alter region --name=/Person --cache-writer=io.pivotal.gemfire.sample.cachewriter.AttributeSyncCacheWriter
 EOF
 
@@ -21,8 +22,8 @@ printf "### Data in Gemfire:\n"
 mvn -Dtest=EntryListingTest test | grep PDX
 
 printf "### Since the cacheWriter is active, any new data added using the blue-app will have all 3 fields - name,firstName,lastName\n"
-curl -X PUT localhost:8080/person/3
-curl -X PUT localhost:8080/person/4
+curl -X PUT localhost:8081/person/3
+curl -X PUT localhost:8081/person/4
 
 echo ""
 mvn -Dtest=EntryListingTest test | grep PDX
@@ -34,8 +35,8 @@ echo ""
 mvn -Dtest=EntryListingTest test | grep PDX
 
 printf "### At this point we can switch to the green-app. Any new data added from the green-app will have all 3 fields as long as the CacheWriter is still active.\n"
-curl -X PUT localhost:8081/person/5
-curl -X PUT localhost:8081/person/6
+curl -X PUT localhost:8082/person/5
+curl -X PUT localhost:8082/person/6
 
 echo "" 
 mvn -Dtest=EntryListingTest test | grep PDX
@@ -54,7 +55,7 @@ mvn -Dtest=EntryListingTest test | grep PDX
 echo ""
 
 printf "### Any new data added or modified from the green-app will now only have the 2 fields - firstName and lastName\n"
-curl -X PUT localhost:8081/person/1
-curl -X PUT localhost:8081/person/7
+curl -X PUT localhost:8082/person/1
+curl -X PUT localhost:8082/person/7
 
 mvn -Dtest=EntryListingTest test | grep PDX
